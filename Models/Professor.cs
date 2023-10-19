41874245eeaae79ed314f3645c584649d4bf32a6
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2023-10-19 11:16:02 -0300

    upadate

---
+++ Models/Professor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public  class Professor: Pessoa
+    {
+        public Professor()
+        {
+        }
+        public Professor(string nome) : base(nome)
+        {
+        }
+        public decimal Salario { get; set; }
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos, sou um professor com salário de {Salario}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using ExemploPOO.Models;
+using ExemploPOO.Interfaces;
+
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Somar(10, 20));
+Console.WriteLine(calc.Subtrair(10, 20));
+Console.WriteLine(calc.Multiplicar(10, 20));
+Console.WriteLine(calc.Dividir(10, 20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Computador c = new Computador();
+// Console.WriteLine(c.ToString());
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa("Cedric");
+// Aluno a1 = new Aluno("Pedro");
+// a1.Apresentar();
+// Professor prof1 = new Professor();
+
+
+
+
+
+
+
+
+
+
+
+// Corrente c = new Corrente();
+// c.Creditar(1000);
+// c.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Aluno a1 = new Aluno();
+// a1.Nome = "Cedric";
+// a1.Idade = 20;
+// a1.Email = "teste@teste.com";
+// a1.Nota = 10;
+// a1.Apresentar();
+
+// Professor p1 = new Professor();
+// p1.Nome = "Marcos";
+// p1.Idade = 30;
+// p1.Salario = 1000;
+// p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(1500);
+// c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa  p1 = new Pessoa();
+// p1.Nome = "Cedric";
+// p1.Idade = 20;
+// p1.Apresentar();
